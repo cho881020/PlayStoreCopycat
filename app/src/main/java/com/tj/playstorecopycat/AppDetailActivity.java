35d700244a37332e203d90f4724283e654e4ba4e
kj_cho <kj_cho@idealidea.co.kr>
2019-04-21 12:30:21 +0900

    앱 상세 액티비티 추가 / 데이터 바인딩 마무리

---
+++ app/src/main/java/com/tj/playstorecopycat/AppDetailActivity.java
@@ -0,0 +1,19 @@
+package com.tj.playstorecopycat;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tj.playstorecopycat.databinding.ActivityAppDetailBinding;
+
+public class AppDetailActivity extends AppCompatActivity {
+
+    ActivityAppDetailBinding act;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        act = DataBindingUtil.setContentView(this, R.layout.activity_app_detail);
+    }
+}

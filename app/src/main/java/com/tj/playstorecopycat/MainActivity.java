9d43578db0b44c0d6ca888f9791e5b9a42ce008e
kj_cho <kj_cho@idealidea.co.kr>
2019-04-21 10:33:37 +0900

    데이터 목록 추가

--- app/src/main/java/com/tj/playstorecopycat/MainActivity.java
+++ app/src/main/java/com/tj/playstorecopycat/MainActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.tj.playstorecopycat.databinding.ActivityMainBinding;
+import com.tj.playstorecopycat.datas.App;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    List<App> appList = new ArrayList<>();
 
     ActivityMainBinding act;
 
@@ -17,9 +22,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         act = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        act.titleTxt.setText("제목 화면");
-
-
+        fillApps();
 
     }
+
+
+    void fillApps() {
+
+        appList.add(new App(1, "아스팔트 8: 에어본", "GameLoft", 5, 6000, true));
+        appList.add(new App(2, "MineCraft - Pocket Edition", "Mojang", 4, 5000, true));
+        appList.add(new App(3, "아스팔트 7: 하트", "GameLoft", 2, 1000, false));
+        appList.add(new App(4, "팔라독(Paladog)", "FazeCat", 3, 1087, false));
+        appList.add(new App(5, "Plants Vs. Zombies", "EA Swiss Sarl", 1, 2000, false));
+        appList.add(new App(6, "스왐피(Swampy)", "Disney", 4, 3000, false));
+
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+

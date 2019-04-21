93fa0172d9a44fa2fa55a528bdb377d570d812c1
kj_cho <kj_cho@idealidea.co.kr>
2019-04-21 12:11:41 +0900

    이벤트 비교용 확인버튼 이벤트 처리

--- app/src/main/java/com/tj/playstorecopycat/MainActivity.java
+++ app/src/main/java/com/tj/playstorecopycat/MainActivity.java
@@ -3,7 +3,9 @@ package com.tj.playstorecopycat;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tj.playstorecopycat.adapters.AppAdapter;
 import com.tj.playstorecopycat.databinding.ActivityMainBinding;
@@ -30,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
         mAppAdapter = new AppAdapter(MainActivity.this, appList);
         act.appRankListView.setAdapter(mAppAdapter);
 
+
+//      Q1. 확인버튼이 눌리면 "확인버튼을 눌렀습니다." 라는 토스트를 띄워봅시다.
+
+        act.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "확인버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
 
 
@@ -41,21 +55,6 @@ public class MainActivity extends AppCompatActivity {
         appList.add(new App(4, "팔라독(Paladog)", "FazeCat", 3, 1087, false));
         appList.add(new App(5, "Plants Vs. Zombies", "EA Swiss Sarl", 1, 2000, false));
         appList.add(new App(6, "스왐피(Swampy)", "Disney", 4, 3000, false));
-
-        appList.add(new App(1, "아스팔트 8: 에어본", "GameLoft", 5, 6000, false));
-        appList.add(new App(2, "MineCraft - Pocket Edition", "Mojang", 4, 5000, false));
-        appList.add(new App(3, "아스팔트 7: 하트", "GameLoft", 2, 1000, false));
-        appList.add(new App(4, "팔라독(Paladog)", "FazeCat", 3, 1087, false));
-        appList.add(new App(5, "Plants Vs. Zombies", "EA Swiss Sarl", 1, 2000, false));
-        appList.add(new App(6, "스왐피(Swampy)", "Disney", 4, 3000, false));
-
-        appList.add(new App(1, "아스팔트 8: 에어본", "GameLoft", 5, 6000, false));
-        appList.add(new App(2, "MineCraft - Pocket Edition", "Mojang", 4, 5000, false));
-        appList.add(new App(3, "아스팔트 7: 하트", "GameLoft", 2, 1000, false));
-        appList.add(new App(4, "팔라독(Paladog)", "FazeCat", 3, 1087, false));
-        appList.add(new App(5, "Plants Vs. Zombies", "EA Swiss Sarl", 1, 2000, false));
-        appList.add(new App(6, "스왐피(Swampy)", "Disney", 4, 3000, false));
-
     }
 
 
